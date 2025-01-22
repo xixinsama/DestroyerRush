1099b686c7e518a357643a85250cd08ee881a929
tyandtd <129674511+tyandtd@users.noreply.github.com>
2025-01-22 15:20:40 +0800

    little design for level2

--- script/player_health.gd
+++ script/player_health.gd
@@ -19,7 +19,7 @@ func update_health(HP_before, HP_now) -> void:
 		for i in HP_before - HP_now:
 			# 如果还有加血的敌方，可能序号就变了？
 			var node_ani = self.get_child(HP_before)
-			print(get_child_count())
-			print(get_children())
+			#print(get_child_count())
+			#print(get_children())
 			
 			node_ani.get_node("AnimationPlayer").play("deduction")
