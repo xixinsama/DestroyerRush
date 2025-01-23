b2ed4f559cdd7779f6c7c16482bd04d2c154a048
xixinsama <xixin142857@outlook.com>
2025-01-23 22:02:27 +0800

    再次修改FPC组件的bug

--- compenent/follow_path_component.gd
+++ compenent/follow_path_component.gd
@@ -4,23 +4,35 @@ extends Node
 
 @export var actor: Node2D
 @export var path_points: Curve2D = null ##绘制曲线，将节点按曲线轨迹移动
-@export var is_around: bool = false ##是否来回移动，周期为到终点后原路返回起点。若is_loop为false，则只来回一次。在终点会原路返回。如果没有curve，则会修改成false 
-@export var is_loop: bool = true ##是否循环。若曲线未闭合，则会直接跳到起点。如果没有curve，则会修改成false 
+@export var is_around: bool = false ##是否来回移动，周期为到终点后原路返回起点。若is_loop为false，则只来回一次。在终点会原路返回。
+@export var is_loop: bool = true ##是否循环。若曲线未闭合，则会直接跳到起点。
 @export var speed: int = 0 ##节点移动的速度
 
 var distance_along_path: float = 0.0
 var going_forward: bool = true # 标识当前是否向前移动
+var is_following: bool = false # 标识是否正在跟随路径
 signal finish_oneloop ##一次路径巡游结束后发送信号, 如果是is_loop为true，则每次都发送信号
 
 func _ready() -> void:
-	if actor != null:
+	if path_points != null and actor != null:
 		actor.tree_exiting.connect(stop_process)
-		if path_points == null:
-			is_around = false
-			is_loop = false
+		start_follow()
 	else:
 		stop_process()
 		return
+
+func start_follow() -> void:
+		if path_points == null:
+			print("没有路径资源")
+			is_around = false
+			is_loop = false
+		is_following = true
+		set_process(true)
+
+# 停止运动
+func stop_process() -> void:
+	is_following = false
+	set_process(false)
 
 func _process(delta: float) -> void:
 	var path_length = path_points.get_baked_length() # 路径总长度
@@ -57,7 +69,3 @@ func _process(delta: float) -> void:
 	# 获取位置
 	var new_position: Vector2 = path_points.sample_baked(distance_along_path)
 	actor.global_position = new_position
-
-# 停止运动
-func stop_process() -> void:
-	set_process(false)
