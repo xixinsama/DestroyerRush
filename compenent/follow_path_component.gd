2f6b70099130b6bc54ff65b1ac26c3fe486125dc
xixinsama <xixin142857@outlook.com>
2025-01-25 15:52:02 +0800

    添加了两种攻击模式
    在level1

--- compenent/follow_path_component.gd
+++ compenent/follow_path_component.gd
@@ -9,7 +9,8 @@ extends Node2D
 @export var is_loop: bool = true ##是否循环。若曲线未闭合，则会直接跳到起点。
 @export var speed: int = 0 ##节点移动的速度
 
-var distance_along_path: float = 0.0
+var distance_along_path: float = 0.0 # 当前位置
+var path_length: float # 总路径长度
 var going_forward: bool = true # 标识当前是否向前移动
 var is_following: bool = false # 标识是否正在跟随路径
 signal finish_oneloop ##一次路径巡游结束后发送信号, 如果是is_loop为true，则每次都发送信号
@@ -33,12 +34,16 @@ func start_follow(progress: float = 0) -> void:
 		set_process(true)
 
 # 停止运动
-func stop_process() -> void:
+func stop_process() -> float:
 	is_following = false
 	set_process(false)
+	return distance_along_path
 
 func _process(delta: float) -> void:
-	var path_length = path_points.get_baked_length() # 路径总长度
+	if !is_following:
+		return
+	# 每次都获取总长，万一有要加点呢？
+	path_length = path_points.get_baked_length() # 路径总长度
 	# print(path_length)
 	# 只去一次
 	if not is_around and not is_loop:
@@ -67,9 +72,17 @@ func _process(delta: float) -> void:
 	# 一直循环走
 	if not is_around and is_loop:
 		distance_along_path += speed * delta
+		# print(distance_along_path)
 		if distance_along_path > path_length:
 			distance_along_path -= path_length
+			finish_oneloop.emit()
+		if distance_along_path < 0:
+			distance_along_path += path_length
 			finish_oneloop.emit()
 	# 获取位置
 	var new_position: Vector2 = path_points.sample_baked(distance_along_path)
 	actor.global_position = new_position
+
+## 获取当前路径总长度
+func get_path_length() -> float: 
+	return path_length
--- script/level_1.gd
+++ script/level_1.gd
@@ -4,45 +4,98 @@ extends Node2D
 @onready var player: Node2D = $player
 @onready var enemy_1: Node2D = $enemy1
 @onready var enemy_2: Node2D = $enemy2
-@onready var follow_path_component_1: FollowPathComponent = $FollowPathComponent
+@onready var follow_path_component: FollowPathComponent = $FollowPathComponent
 @onready var follow_path_component_2: FollowPathComponent = $FollowPathComponent2
-@onready var bullet: Bullet = $Node2D/bullet
-@onready var bullet_2: Bullet = $Node2D/bullet2
-@onready var bullet_3: Bullet = $Node2D/bullet3
-@onready var bullet_4: Bullet = $Node2D/bullet4
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent
+
+var attack_method1: Timer
+var attack_method2: Timer
+var attack_method3: Timer
 
 func _ready() -> void:
-	#player.tree_exited.connect(func():
-		#if enemy == null: # 不要动
-			#return
-		#else:
-			#await get_tree().create_timer(1.0).timeout
-			#get_tree().change_scene_to_file("res://scene/game_over.tscn")
-		#)
-	#enemy.tree_exited.connect(func():
-		#if player == null:
-			#return
-		#else:
-			#await get_tree().create_timer(1.0).timeout
-			#get_tree().change_scene_to_file("res://Levels/level_2.tscn") # 改这里
-		#)
-	#follow_path_component_1.start_follow(1)
-	#follow_path_component_2.start_follow(1505)
-	# get_node()并初始化
-	#var bf1: FollowPathComponent = bullet.get_node("FollowPathComponent")
-	#bf1.path_points = preload("res://asset/PathSrc/path_circle.tres")
-	#bf1.start_follow()
-	#bf1.speed = 200
-	# 
-	# bullet.initialize()
+	player.tree_exited.connect(_on_player_exited)
+	enemy_1.tree_exited.connect(_on_enemy1_exited)
+	enemy_2.tree_exited.connect(_on_enemy2_exited)
+	follow_path_component.start_follow()
+	follow_path_component_2.start_follow()
 	
-	#bullet_2.path_points = preload("res://asset/PathSrc/path_circle.tres") #失败
-	#bullet_2.start_follow() # 成功
-	#bullet_2.speed = 250
-	#bullet_3.path_points = preload("res://asset/PathSrc/path_circle.tres")
-	##bullet_3.start_follow()
-	#bullet_2.speed = 300
-	#bullet_4.path_points = preload("res://asset/PathSrc/path_circle.tres")
-	##bullet_4.start_follow()
-	#bullet_2.speed = 350
+	attack_method1 = Timer.new()
+	add_child(attack_method1)
+	attack_method1.wait_time = 6.0
+	attack_method1.timeout.connect(attack_1)
+	attack_method1.start()
+	
+	attack_method2 = Timer.new()
+	add_child(attack_method2)
+	attack_method2.wait_time = 8.0
+	attack_method2.timeout.connect(attack_2)
+	attack_method2.start()
+	
+	attack_method3 = Timer.new()
+	add_child(attack_method3)
+	attack_method3.wait_time = 12.0
+	attack_method3.timeout.connect(attack_3)
+	attack_method3.start()
+
+func _process(delta: float) -> void:
+	# 根据玩家位置上传敌人位置信息至全局
+	if player != null:
+		if player.global_position.x < 360:
+			Status.enemy_position = enemy_1.global_position
+		elif player.global_position.x >= 360:
+			Status.enemy_position = enemy_2.global_position
+		else:
+			print("玩家位于三界之外")
+			return
+
+func _on_player_exited() -> void:
+	if enemy_1 or enemy_2 == null: # 不要动
+			return
+	else:
+		await get_tree().create_timer(1.0).timeout
+		set_process(false)
+		get_tree().change_scene_to_file("res://scene/game_over.tscn")
+
+func _on_enemy1_exited() -> void:
 	pass
+
+func _on_enemy2_exited() -> void:
+	pass
+
+# 全局下攻击
+func attack_1() -> void:
+	var line_down: Bullet # 一排竖着随机水平位置往下落
+	var num: int = 8 ##子弹数量
+	var speed: int = 150 ##子弹速度
+	var frame_bullet = 5 ##子弹样式
+	for i in range(0,num):
+		line_down = spawner_component.spawn(Vector2(25 + i * round(720 / num) +randi_range(20,50) , -50 + randi_range(-50,50) ),self,0)
+		line_down.frame = frame_bullet
+		line_down.velocity = Vector2(0,speed)
+		line_down.initialize()
+
+# 敌人二的攻击
+func attack_2() -> void:
+	var direct_follow: Bullet # 瞬间跟踪并很快的往玩家身上射
+	var num: int = 40 ##子弹数量
+	var speed: int = 600 ##子弹速度
+	var frame_bullet = 16 ##子弹样式
+	for i in range(0,num):
+		var offset: Vector2 = Vector2(randi_range(-4,4), randi_range(-4,4))
+		direct_follow = spawner_component.spawn(enemy_2.global_position + Vector2(0 ,48) + offset, self, 0)
+		direct_follow.frame = frame_bullet
+		direct_follow.speed_trail_2 = speed
+		direct_follow.initialize()
+		await get_tree().create_timer(0.05).timeout
+
+# 敌人一的攻击
+func attack_3() -> void:
+	var scatter: Bullet # 散射
+	var num: int = 15 ##子弹数量
+	var speed: int = 250 ##子弹速度
+	var frame_bullet = 7 ##子弹样式
+	for i in range(0,num):
+		scatter = spawner_component.spawn(enemy_1.global_position + Vector2(0 ,48), self, 0)
+		scatter.frame = frame_bullet
+		scatter.velocity = speed * Vector2(sin(i) ,abs(cos(i)))
+		scatter.initialize()
